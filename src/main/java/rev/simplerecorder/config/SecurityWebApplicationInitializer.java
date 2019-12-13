9cbb38cd1ea80dcd522b4e76472349a91f07ef44
UEDA Hideki <hideki19700413@gmail.com>
2019-12-14 08:37:31 +0900

    first commit

---
+++ src/main/java/rev/simplerecorder/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package rev.simplerecorder.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+}
+
---
+++ src/main/java/rev/simplerecorder/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package rev.simplerecorder.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity(debug=true)
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		System.out.println("WebSecurityConfig::UserDetailsService()");
+
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+		return manager;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		System.out.println("WebSecurityConfig::configre()");
+		http
+			.authorizeRequests()
+				.antMatchers("/login*").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+			    //.loginPage("/login")
+				.and()
+			.httpBasic();
+		http.csrf().disable();
+	}
+	
+}
